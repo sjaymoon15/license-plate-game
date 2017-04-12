5fdc325ee7e9b649b17e82f5aaf934b3d42d53e8
Seungjae Moon <sjaymoon15@gmail.com>
2017-04-12 16:40:23 -0700

    style texts in gameHistory

--- src/components/GameHistory.js
+++ src/components/GameHistory.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, TouchableHighlight } from 'react-native';
 import MapView from 'react-native-maps';
 import { Card, CardSection, Button } from './common';
 
 class GameHistory extends Component {
   componentDidMount() {
     this.refs.map.fitToElements(true);
+  }
+  onRowPress(row) {
+    console.log(row);
   }
   renderMapView() {
     const { stateData } = this.props.realtimeGame;
@@ -44,6 +47,25 @@ class GameHistory extends Component {
             />
           ))}
         </MapView>
+        <ScrollView style={styles.scrollViewStyle}>
+          {foundStateData.map((foundState) => {
+            return (
+              <View key={foundState.key}>
+                <CardSection style={styles.containerStyle}>
+                  <Text style={styles.contentStyle}>
+                    {foundState.updatedAt}
+                  </Text>
+                  <Text style={styles.contentStyle}>
+                    {foundState.name}
+                  </Text>
+                  <Text style={styles.contentStyle}>
+                    {foundState.seenBy}
+                  </Text>
+                </CardSection>
+              </View>
+            );
+          })}
+        </ScrollView>
       </View>
     );
   }
@@ -67,6 +89,20 @@ const styles = {
     left: 20,
     right: 20,
     top: 20
+  },
+  scrollViewStyle: {
+    position: 'absolute',
+    height: 250,
+    left: 20,
+    right: 20,
+    bottom: 20
+  },
+  containerStyle: {
+    justifyContent: 'space-between'
+  },
+  contentStyle: {
+    fontSize: 15,
+    alignSelf: 'flex-start'
   }
 };
 const mapStateToProps = (state) => {
